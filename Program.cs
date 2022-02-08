529a1df0dfc319f1dfe76a1de2438d707f807a62
Janaki2425 <96913266+Janaki2425@users.noreply.github.com>
2022-02-08 16:02:02 +0530

    Main program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ThirdPartyAccess
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CsvHandler.ImplementCSVDataHandling();
+        }
+    }
+}
